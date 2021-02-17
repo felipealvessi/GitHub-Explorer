fef56fb52d83d66826c15192f35255497877d7f7
Felipe Alves <felipealvesinfor@gmail.com>
2021-02-17 01:32:24 -0300

    criado a pasta pages

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const Dashaboard = () => { }
